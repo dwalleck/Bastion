dbacf8c5608e27facb95e6312bca534e9341352e
Daryl Walleck <daryl.walleck@rackspace.com>
2015-01-13 16:17:53 -0600

    Added image usage models

---
+++ Bastion/Models/Reaper/ImageUsage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bastion.Models.Reaper
+{
+    public class ImageUsage : IResourceUsage
+    {
+        public int Images { get; set; }
+        public long DiskUsed { get; set; }
+
+        public UsageStatus CurrentUsageStatus()
+        {
+            if (Images < 10)
+            {
+                return UsageStatus.Ok;
+            }
+            else if (Images < 25)
+            {
+                return UsageStatus.Warning;
+            }
+            else
+            {
+                return UsageStatus.OverLimit;
+            }
+        }
+    }
+}
